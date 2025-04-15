04e286f5ddef407a73eda4df678e189213900498
Łukasz Hałat <lukaszhalat84@gmail.com>
2025-04-15 13:58:53 +0200

    Utworzenie projektu

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+/**
+ * Fetches data from API.
+ * @param {string} url - URL adress of API.
+ */
+async function getData(url) {
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+/**
+ * Displays data from API in HTML table.
+ * @param {string} url - URL adress of API.
+ * @param {string} element - HTML element to table display.
+ * @param {string[]} customHeaders - optional custom table headers array.
+ */
+async function displayPrices(url, element, customHeaders = []) {
+    const prices = await getData(url);
+    const pricesElement = document.getElementById(element);
+    let dataToDisplay = prices;
+
+    if (prices && prices.hasOwnProperty("rates") && Array.isArray(prices.rates)) {
+        dataToDisplay = prices.rates;
+    }
+
+    if (pricesElement && dataToDisplay.length > 0) {
+        const headers = Object.keys(dataToDisplay[0]);
+        let tableHTML = `
+        <table class="table table-bordered table-striped w-auto">
+          <thead>
+            <tr class="table-light">
+      `;
+
+        if (Array.isArray(customHeaders) && customHeaders.length > 0) {
+            customHeaders.forEach(header => {
+                tableHTML += `<th>${header}</th>`;
+            });
+        } else {
+            headers.forEach(header => {
+                tableHTML += `<th>${header}</th>`;
+            });
+        }
+
+
+        tableHTML += `
+            </tr>
+          </thead>
+          <tbody>
+      `;
+
+        dataToDisplay.forEach(item => {
+            tableHTML += `<tr class="table-light">`;
+            headers.forEach(header => {
+                tableHTML += `<td>${item[header]}</td>`;
+            });
+            tableHTML += `</tr>`;
+        });
+
+        tableHTML += `
+          </tbody>
+        </table>
+      `;
+
+        pricesElement.innerHTML = tableHTML;
+    } else if (!pricesElement) {
+        console.log(`Element o id ${element}' nie został znaleziony na stronie.`);
+    } else if (dataToDisplay.length === 0) {
+        console.log("Tablica danych do wyświetlenia jest pusta.");
+    }
+}
